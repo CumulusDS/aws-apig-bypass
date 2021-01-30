930c60993ab73e8a9b78e0bfff72788b6cba19c4
Joshua Napoli <josh@cumulusds.com>
2021-01-29 22:10:52 -0500

    Add all options to createAPIGatewayEvent

--- src/create-api-gateway-event.js
+++ src/create-api-gateway-event.js
@@ -5,55 +5,71 @@ import type { APIGatewayEvent } from "flow-aws-lambda";
 export type PathParameters = { [name: string]: string } | null;
 export type QueryStringParameters = { [name: string]: string } | null;
 
+const defaultRequestContext = {
+  accountId: "",
+  apiId: "",
+  httpMethod: "",
+  identity: {
+    accessKey: null,
+    accountId: null,
+    apiKey: null,
+    caller: null,
+    cognitoAuthenticationProvider: null,
+    cognitoAuthenticationType: null,
+    cognitoIdentityId: null,
+    cognitoIdentityPoolId: null,
+    sourceIp: "",
+    user: null,
+    userAgent: null,
+    userArn: null
+  },
+  stage: "",
+  requestId: "",
+  resourceId: "",
+  resourcePath: ""
+};
+
 export type CreateAPIGatewayEventOptions = {
   body?: string,
+  headers?: { [name: string]: string },
+  multiValueHeaders?: { [name: string]: string[] },
+  httpMethod?: string,
+  isBase64Encoded?: boolean,
   path?: string,
   pathParameters?: PathParameters,
   queryStringParameters?: QueryStringParameters,
-  isBase64Encoded?: boolean
+  multiValueQueryStringParameters?: { [name: string]: string[] },
+  stageVariables?: { [name: string]: string },
+  requestContext?: typeof defaultRequestContext,
+  resource?: string
 };
 
 export default function createAPIGatewayEvent({
+  body = null,
+  headers = {},
+  multiValueHeaders = {},
+  httpMethod = "",
+  isBase64Encoded = false,
   pathParameters = null,
   path = "",
-  body = null,
   queryStringParameters = null,
-  isBase64Encoded = false
+  multiValueQueryStringParameters = null,
+  stageVariables = null,
+  requestContext = defaultRequestContext,
+  resource = ""
 }: CreateAPIGatewayEventOptions = {}): APIGatewayEvent<string> {
   return {
     body,
-    headers: {},
-    multiValueHeaders: {},
-    httpMethod: "",
+    headers,
+    multiValueHeaders,
+    httpMethod,
     isBase64Encoded,
     path,
     pathParameters,
     queryStringParameters,
-    multiValueQueryStringParameters: null,
-    stageVariables: null,
-    requestContext: {
-      accountId: "",
-      apiId: "",
-      httpMethod: "",
-      identity: {
-        accessKey: null,
-        accountId: null,
-        apiKey: null,
-        caller: null,
-        cognitoAuthenticationProvider: null,
-        cognitoAuthenticationType: null,
-        cognitoIdentityId: null,
-        cognitoIdentityPoolId: null,
-        sourceIp: "",
-        user: null,
-        userAgent: null,
-        userArn: null
-      },
-      stage: "",
-      requestId: "",
-      resourceId: "",
-      resourcePath: ""
-    },
-    resource: ""
+    multiValueQueryStringParameters,
+    stageVariables,
+    requestContext,
+    resource
   };
 }
