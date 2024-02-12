ce2eb34298808cae722d4cb75fa26830a9f382d3
Joshua Napoli <josh@cumulusds.com>
2024-02-12 13:11:02 -0500

    Export the APIGatewayHandlerClient type

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // @flow
 
 export { default as createClient } from "./create-client";
+export type { APIGatewayHandlerClient } from "./create-client";
 export { default as createAPIGatewayEvent } from "./create-api-gateway-event";
